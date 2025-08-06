08cba90908193743f57d29d64350537c0944eb7b
JMclean-ops <JMclean@gameday.pro>
2025-08-06 14:27:28 -0400

    Delete components/ui directory

--- components/ui/progress.tsx
+++
@@ -1,8 +0,0 @@
-
-export function Progress({ value }: { value: number }) {
-  return (
-    <div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
-      <div className="bg-green-500 h-4" style={{ width: `${value}%` }} />
-    </div>
-  );
-}
